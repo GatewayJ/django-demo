83e7fadd995e36662a83013a7b76c052208d01aa
jihognwei <835269233@qq.com>
2020-03-01 16:31:37 +0800

    docker 共享文件路径

--- start_update.sh
+++ start_update.sh
@@ -3,4 +3,4 @@ git pull origin master
 docker stop blog_admin
 docker rm blog_admin
 docker build -t blog/centos:latest .
-docker run --name  blog_admin -d   -p 80:80    blog/centos:latest
+docker run --name  blog_admin -d   -p 80:80 -v /data/blog_images:/home/www/blog/media  blog/centos:latest
