10374d48855cfd0a39ed99260391bf87df87d420
jhw <jhw@localhost.localdomain>
2020-05-05 17:25:11 +0800

    返回标签

--- blog/api.py
+++ blog/api.py
@@ -1,6 +1,9 @@
 # coding:utf-8
-from rest_framework import viewsets, mixins
+from rest_framework import mixins
+from rest_framework import viewsets
+from rest_framework.response import Response
 from blog.serializers import ArticalSerializer
+from blog.serializers import ArticalListSerializer
 from blog.models import Artical
 
 
@@ -9,3 +12,15 @@ class ArticleList(viewsets.ReadOnlyModelViewSet):
     serializer_class = ArticalSerializer
     ordering_fields = ['pushed', ]
     lookup_field = 'title'
+
+
+    def list(self, request, *args, **kwargs):
+        self.serializer_class = ArticalListSerializer
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
--- blog/serializers.py
+++ blog/serializers.py
@@ -1,20 +1,27 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from blog.models import Artical
+from blog.models import ArticalTag
 
 # Serializers define the API representation.
 
+class ArticalTagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ArticalTag
+        fields = ('id', 'tag_content')
 
 class ArticalSerializer(serializers.ModelSerializer):
     pushed = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    articaltag = ArticalTagSerializer(many = True)
     class Meta:
         model = Artical
-        fields = ('id','title','summary','content','pushed')
+        fields = ('id', 'title', 'content', 'articaltag', 'pushed')
 
 
-
-class ImageField(serializers.ImageField):
-    """
-    从 easy_thumbnails.fields.ThumbnailerImageField 字段类型中解析出缩略图信息
-    """
-    pass
+class ArticalListSerializer(serializers.ModelSerializer):
+    pushed = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    articaltag = ArticalTagSerializer(many = True)
+    class Meta:
+        model = Artical
+        fields = ('id', 'title', 'summary', 'articaltag', 'pushed')
